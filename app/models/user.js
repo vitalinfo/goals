eff41741e4ad9005bd6d1dc3034414965c47aba3
Vital Ryabchinskiy <vital.ryabchinskiy@gmail.com>
2017-07-12 21:04:56 +0000

    Create user

--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,16 @@
 import DS from 'ember-data';
+import Validator from '../mixins/model-validator';
 
-export default DS.Model.extend({
+export default DS.Model.extend(Validator, {
   first_name: DS.attr('string'),
-  last_name: DS.attr('string')
+  last_name: DS.attr('string'),
+  average_score: DS.attr('number'),
+  validations: {
+    first_name: {
+      presence: true
+    },
+    last_name: {
+      presence: true
+    }
+  }
 });
