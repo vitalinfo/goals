1ea0b31163fa27e47c6a647557276180d22f7204
Vital Ryabchinskiy <vital.ryabchinskiy@gmail.com>
2017-07-13 20:24:17 +0000

    Basic model tests

--- tests/unit/models/goal-test.js
+++ tests/unit/models/goal-test.js
@@ -1,12 +1,38 @@
+import Ember from 'ember';
 import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('goal', 'Unit | Model | goal', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: ['model:user']
 });
 
 test('it exists', function(assert) {
   let model = this.subject();
   // let store = this.store();
   assert.ok(!!model);
+});
+
+test('should require fields', function(assert) {
+  let goal = this.subject();
+  Ember.run(() => goal.validate());
+  assert.equal(goal.get('errors').errorsFor('title').length, 1);
+  assert.equal(goal.get('errors').errorsFor('score').length, 2);
+  assert.equal(goal.get('errors').errorsFor('user').length, 1);
+});
+
+test('should validate score', function(assert) {
+  let goal = this.subject({score: -1});
+  Ember.run(() => goal.validate());
+  assert.equal(goal.get('errors').errorsFor('score').length, 1);
+  Ember.run(() => {
+    goal.set('score', 11);
+    goal.validate()
+  });
+  assert.equal(goal.get('errors').errorsFor('score').length, 1);
+  Ember.run(() => {
+    goal.set('score', 5);
+    goal.validate()
+  });
+  Ember.run(() => goal.validate());
+  assert.equal(goal.get('errors').errorsFor('score').length, 0);
 });
--- tests/unit/models/user-test.js
+++ tests/unit/models/user-test.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('user', 'Unit | Model | user', {
@@ -9,4 +10,21 @@ test('it exists', function(assert) {
   let model = this.subject();
   // let store = this.store();
   assert.ok(!!model);
+});
+
+test('should concat first and last name', function(assert) {
+  let user = this.subject({first_name: 'John', last_name: 'Doe'});
+  assert.equal(user.get('name'), 'John Doe');
+});
+
+test('should require first_name', function(assert) {
+  let user = this.subject({last_name: 'Doe'});
+  Ember.run(() => user.validate());
+  assert.equal(user.get('errors').errorsFor('first_name').length, 1);
+});
+
+test('should require last_name', function(assert) {
+  let user = this.subject({first_name: 'John'});
+  Ember.run(() => user.validate());
+  assert.equal(user.get('errors').errorsFor('last_name').length, 1);
 });
